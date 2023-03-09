fb13eb8e4307bb6e6cf82f46b554b4398da24ab2
Muhamad Nur Asyraaf <masyraaf14@gmail.com>
2023-03-10 01:56:02 +0800

    no home cmponent displayed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { BrowserRouter , Routes , Route, Link} from 'react-router-dom'
 import './App.css';
-
+import Navbar from './components/Navbar'
+import  Home   from './pages/Home'
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+         <Link to="/"> Home </Link>
         <Routes >
-          <Route />
+          <Route path='/' element={<Home />}/>
+          <Route path='/*' />
         </Routes>
-      
       </BrowserRouter>
 
     </div>
